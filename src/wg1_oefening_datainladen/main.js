fdf52b8145b45627130b346baf22de502afcc25c
Nick Rutten <hello@nickrttn.me>
2016-09-10 20:46:01 +0200

    finish data loading exercise

--- src/wg1_oefening_datainladen/main.js
+++ src/wg1_oefening_datainladen/main.js
@@ -1,2 +1,20 @@
-
 // Laad hier de data in
+d3.json('../data/data_rijksmonumenten.json', data => {
+	console.log(data);
+});
+
+d3.html('../data/data_buurtnamenEnschede.html', data => {
+	console.log(data);
+});
+
+// ssv stands for Semicolon Separated Values
+const ssv = d3.dsvFormat(';');
+d3.request('../data/data_hotelsAmsterdam.csv')
+  .mimeType('text/plain')
+	.get(data => {
+		console.log(ssv.parse(data.response));
+	});
+
+d3.xml('../data/data_veiligstallen.xml', data => {
+	console.log(data);
+})
