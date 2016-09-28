92067f50ab1ce6ccb600cb741a6b76beb7d23116
Nick Rutten <hello@nickrttn.me>
2016-09-28 21:08:31 +0200

    started work on prototyping for the second FED3 assignment

--- src/opdracht_2/main.js
+++ src/opdracht_2/main.js
@@ -3,105 +3,107 @@
 const defaultMargin = 48;
 
 // DOM stuff
-const chartWidth = document.querySelector('.bar-chart').clientWidth;
-const chartHeight = document.querySelector('.bar-chart').clientHeight;
+const chartWidth = document.querySelector('.drawing-area').clientWidth;
+const chartHeight = document.querySelector('.drawing-area').clientHeight;
 const w = chartWidth - (defaultMargin * 2) - (defaultMargin * 2);
 const h = chartHeight - (defaultMargin * 2) - (defaultMargin * 2);
 
 // D3.js selectors
-const chart = d3.select('.bar-chart');
+const svg = d3.select('.drawing-area');
 
 // Create a D3 queue
 const q = d3.queue();
 
 // Add JSON files to the queue
 q.defer(d3.json, 'mood.json');
-q.defer(d3.json, 'steps.json');
+q.defer(d3.csv, 'kit.csv');
 
-q.await((error, mood, steps) => {
-  // Filter all the objects that have null for a value
-  // and concatenate the two files together in a very ugly
-  // nested loop
-  const data = []
-
-  mood.forEach(moodObj => {
-    if (moodObj.value) {
-      const obj = {
-        date: Date(moodObj.date),
-        mood: Number(moodObj.value),
-      };
-
-      steps.forEach(stepsObj => {
-        if (stepsObj.date === obj.date) {
-          obj.steps = Number(stepsObj.value);
-        }
+q.await((error, mood, kit) => {
+  // Filter and normalize the mood data by
+  // removing null values and renaming some stuff
+  // to make the keys less generic.
+  const moodProcessed = [];
+  mood.forEach((obj) => {
+    if (obj.value) {
+      moodProcessed.push({
+        date: new Date(obj.date),
+        mood: parseInt(obj.value, 10),
       });
-
-      data.push(obj);
     }
   });
 
-  // Create real date objects from simple date strings
-  const dateFormat = d3.timeFormat('%a %e');
-  const dates = moodFiltered.map(obj => new Date(obj.date));
-
-  // Coerce values to numbers
-  const geluk = moodFiltered.map(obj => Number(obj.value));
-  const stappen = stepsFiltered.map(obj => Number(obj.value));
-
-  // And put it back together
-  const coercedData = { dates, geluk, stappen };
-
-  // Set up bar styles
-  const barWidth = chartWidth / (dates.length * 4);
-
-  // Create axis' functions and orientations
-  const x = d3.scaleTime().domain([d3.min(coercedData.dates), d3.max(coercedData.dates)]).range([0, w]);
-  const y1 = d3.scaleLinear().domain([0, d3.max(coercedData.geluk)]).range([h, 0]);
-  const y2 = d3.scaleLinear().domain([0, d3.max(coercedData.stappen)]).range([h, 0]);
-
-  const xAxis = d3.axisBottom(x).tickFormat(dateFormat);
-  const y1Axis = d3.axisLeft(y1).ticks(5);
-  const y2Axis = d3.axisRight(y2).ticks(5);
-
-  // Add axis' to the SVG
-  chart.append('g')
-       .attr('class', 'x-axis')
-       .attr('transform', `translate(${barWidth}, ${h + barWidth})`)
-       .call(xAxis)
-       .selectAll('.x-axis text')
-       .attr('transform', `translate(${barWidth/2}, 0)`);
-
-  chart.append('g')
-       .attr('class', 'y1-axis')
-       .attr('transform', `translate(${barWidth}, ${barWidth})`)
-       .call(y1Axis);
-
-  chart.append('g')
-       .attr('class', 'y2-axis')
-       .attr('transform', `translate(${w + defaultMargin}, ${barWidth})`)
-       .call(y2Axis);
-
-  // Draw the actual bars and labels
-  Object.keys(coercedData).forEach((key, index) => {
-    const bars = chart.selectAll(`.data-${key}`)
-      .data(coercedData[key])
-      .enter().append('g')
-        .attr('class', `data-${key}`)
-        .attr('transform', (d, i) => (index ? `translate(${x(dates[i]) + barWidth}, 0)` :
-                                                     `translate(${x(dates[i]) + (barWidth * 1.75)}, 0)`));
-                                             // This works because 0 is falsy
-
-    bars.append('rect')
-        .attr('y', d => (key === 'geluk' ? y1(d) + barWidth : y2(d) + barWidth))
-        .attr('height', d => (key === 'geluk' ? h - y1(d) : h - y2(d)))
-        .attr('width', barWidth);
-
-    chart.append('text')
-     .attr('class', `${key}-label`)
-     .attr('transform', () => (key === 'geluk' ? `translate(0, ${barWidth/2})` : `translate(${w}, ${barWidth/2})`))
-     .attr('fill', () => (key === 'geluk' ? 'mediumblue' : 'crimson'))
-     .text(key)
-     .attr('font-size', '9');
+  // Filter and normalize data about Kit by
+  // removing some (for now) unneeded cruft and
+  // renaming some stuff to English for consistency.
+  const kitProcessed = [];
+  kit.forEach((obj) => {
+    if (obj.gevoel) {
+      kitProcessed.push({
+        date: new Date(obj.datum),
+        mood: parseInt(obj.gevoel, 10),
+        activity: obj.activiteit,
+        location: obj.locatie,
+      });
+    }
   });
+
+  // For the first chart, count how often a
+  // certain mood occurs in moodProcessed.
+  const moodCounted = [];
+  moodProcessed.forEach((obj) => {
+    const countIndex = moodCounted.findIndex(countObj => countObj.mood == obj.mood);
+
+    // If we get into this conditional,
+    // halt execution of the loop.
+    if (countIndex === -1) {
+      return moodCounted.push({
+        mood: obj.mood,
+        count: 1,
+      });
+    }
+
+    // If we don't get into the conditional,
+    // simply add 1 to the relevant count.
+    // Return false to appease eslint.
+    moodCounted[countIndex].count += 1;
+    return false;
+  });
+
+  // Create a D3 hierarchy out of my general mood data.
+  const hierarchicalCount = d3.hierarchy({ children: moodCounted })
+    .sum(d => d.count).sort(d => d.mood);
+
+  // Create a packing layout function based on my chart
+  const bubble = d3.pack()
+    .size([chartHeight, chartHeight])
+    .padding(defaultMargin / 3);
+
+  // Pack the hierarchical data (give it x/y and r values)
+  bubble(hierarchicalCount);
+
+  // Add the first circle packing layout to the drawing area.
+  // Mood Circles sounds like a great name for a product.
+  const moodCircles = svg.append('g')
+    .attr('class', 'whole-day')
+    .selectAll('g')
+    .data(hierarchicalCount.children)
+    .enter().append('g')
+    .attr('transform', d => `translate(${d.x}, ${d.y})`)
+    .attr('class', 'node');
+
+  moodCircles.append('circle')
+    .attr('class', d => `mood-${d.data.mood}`)
+    .attr('id', d => `node-${d.data.mood}`)
+    .attr('r', d => d.r);
+
+  moodCircles.append('clipPath')
+    .attr('id', d => `clip-${d.data.mood}`)
+    .append('use')
+    .attr('xlink:href', d => `#node-${d.data.mood}`);
+
+  moodCircles.append('text')
+    .attr('clip-path', d => `url(#clip-${d.data.mood})`)
+    .attr('y', 10)
+    .attr('text-anchor', 'middle')
+    .text(d => `${Math.round((d.data.count / 52) * 100)}%`);
 });
